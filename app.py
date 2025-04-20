3c6b231fa52c9a76133c90e9c1d8c6e00f98326a
Ben Nalevanko <ben.nalevanko@gmail.com>
2025-04-20 14:33:08 -0400

    Polishing stuff, it's working now

--- app.py
+++ app.py
@@ -1,33 +1,38 @@
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
-from flask import jsonify, Flask, request
+from flask import jsonify, Flask, request, render_template
 from flask_cors import CORS
 import csv
 import os
+import uuid
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static/')
 CORS(app)
 
 data = pd.DataFrame()
 UPLOAD_FOLDER = "data_files"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
-@app.route("/load_data", methods=["GET", "POST"])
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/load_data", methods=["POST"])
 def load_data():
-	# downloads the file locally so the app can open it
+	# downloads the file locally so the app can open it, not strictly necessary.
 	if 'file' not in request.files:
 		return jsonify({'message': 'No file part'}), 400
 	file = request.files['file']
 	if file.filename == '':
 		return jsonify({'message': 'No selected file'}), 400
 	if file:
-		filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+		filename=file.filename
+		filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
 		file.save(filepath)
 
 
-	# loads contents into data
+	# loads contents into data, probably not a good idea. Should remove.
 	try:
 		print("try")
 		global data
@@ -60,15 +65,26 @@ def lin_reg_test(trained_model, target, features):
 # Called by script.js
 @app.route("/run", methods=["GET", "POST"])
 def run():
+	requestData = request.get_json()
+	print(requestData)
+	filename = requestData["file"]
+	print(filename)
+	filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+	data = pd.read_csv(filepath)
+
+	#Tempory for testing
+	features = ["area", "bedrooms", "bathrooms", "stories"]
+	target = "price"
+
 	if data is None:
 		return jsonify({"error": "No data Loaded"}), 404
 	#dummy values for testing, call training and testing algorithms
 	match request.json.get("algorithm"):
 		case "linreg":
-			print("linreg")
+			lin_reg_train(data, target, features)
 		case "cluster":
 			print("cluster")
-
+	return jsonify({"text": "Finished"})
 #load_data("test.csv")
 #target = "price"
 #features=["area", "bedrooms", "bathrooms", "stories"]
